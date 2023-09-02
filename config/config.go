48ed1b1af718dfa9d82ba15f40983cc14afb7732
DOO-DEV <nimo.vimo.yank@gmail.com>
2023-09-02 12:27:00 +0330

    remove game scheduler

--- config/config.go
+++ config/config.go
@@ -3,7 +3,6 @@ package config
 import (
 	"game-app/adapter/redis"
 	"game-app/repository/mysql"
-	gamescheduler "game-app/scheduler/game"
 	"game-app/scheduler/match_waited_user"
 	"game-app/service/authservice"
 	"game-app/service/gameservice"
@@ -22,7 +21,6 @@ type Config struct {
 	PresenceService presenceservice.Config   `koanf:"presence_service"`
 	Scheduler       match_waited_user.Config `koanf:"scheduler"`
 	Debug           bool                     `koanf:"debug"`
-	GameScheduler   gamescheduler.Config     `koanf:"game_scheduler"`
 	GameSvc         gameservice.Config       `json:"game_svc"`
 }
 
